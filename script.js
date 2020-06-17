81ecc61a0f13c73c3fcd463893988e45cfdc1f3d
Yesenia Ibarra <yessyibarra17@gmail.com>
2020-06-16 21:23:08 -0700

    Added moment method to get current date and used JQuery to append to p-tag.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//Adding my moment method here to show current date
+console.log(moment());
+let m = moment();
+console.log(m.format("dddd , MMMM Do YYYY"));
+let m2 = m.format("dddd , MMMM Do YYYY");
+
+
+//Using JQUERY to append the current day to the HTML <p-tag>
+$("#currentDay").text(m2);
